5afd07a346c5d7f5bd6811cb88840cd8a0de3581
shams mosowi <shams.mosowi@gmail.com>
2018-06-23 17:24:47 +1000

    education and experince card

---
+++ src/components/EduExpCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import InputWrapper from "./InputWrapper";
+import { Typography, Card, Grid ,IconButton} from "@material-ui/core";
+import EditIcon from '@material-ui/icons/Edit'
+
+const styles = theme => ({
+  root: theme.mixins.gutters({
+    width: 400,
+    paddingTop: 30,
+    paddingBottom: 20,
+  })
+});
+function EduExpCard(props) {
+  const { classes, title, label, description, startDate, endDate } = props;
+  return (
+    <Card className={classes.root}>
+      <Grid container direction="column" alignItems="flex-start" spacing={16}>
+      <Grid container direction="row" alignItems="center" justify="space-between">
+      <Typography variant='subheading'>{title}</Typography>
+      <IconButton className={classes.button} aria-label="Delete">
+        <EditIcon />
+      </IconButton>
+      </Grid>
+        <Grid container direction="row" alignItems="flex-start" justify="space-between">
+          <Typography variant="body1">{label}</Typography>
+          <Typography variant="body1">
+            {startDate} - {endDate}
+          </Typography>
+        </Grid>
+        <Typography variant="body1">{description}</Typography>
+      </Grid>
+    </Card>
+  );
+}
+
+EduExpCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  default: PropTypes.string,
+  placeholder: PropTypes.string,
+  numberOfLines: PropTypes.number,
+  characterLimit: PropTypes.number,
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(EduExpCard);
