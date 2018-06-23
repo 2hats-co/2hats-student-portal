a58027b40247c1943bfef206c1ae14c7e383ee9f
shams mosowi <shams.mosowi@gmail.com>
2018-06-23 17:27:00 +1000

    adding props multiLine textfield

--- src/components/MultiLineTextField.js
+++ src/components/MultiLineTextField.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import InputWrapper from './InputWrapper';
+import { Typography } from '@material-ui/core';
 
 const styles = theme => ({
     root: theme.mixins.gutters({
       marginTop:'10px',
       padding:'11px !important',
       height:'94px',
-      width: '400px',
+     // width: '400px',
       backgroundColor:'#EDEDED',
       border: '1px solid #fff',
       outline: 'none',
@@ -23,22 +24,27 @@ const styles = theme => ({
         }),
   });
 function MultiLineTextField(props) {
-    const { classes,title} = props;
+    const { classes,title,label,hint,placeholder,characterLimit,numberOfLines} = props;
     return(
+      <div>
+      {label&& <Typography variant='caption'>{label}</Typography>}
       <InputWrapper
-      title={title}>
-      <textarea className={classes.root}rows="4" cols="50">
-      For example: 
-Hard-working student (80 WAM) with 3 months experience of UI design internship. I have more than 1-year of experience in using the Adobe creative suite tools, such as Adobe Photoshop and Adobe XD and would like to further utilise such skills in my future position.
-      </textarea>
-      </InputWrapper>
+      title={title}
+      hint={hint}
+      child={(<textarea className={classes.root}rows="4" cols="50">
+      {placeholder} 
+      </textarea>)}
+      />
+      </div>
+
     )
 }
 MultiLineTextField.propTypes = {
   title: PropTypes.string,
   hint: PropTypes.string,
+  label:PropTypes.string,
   default:PropTypes.string, 
-  placeHolder: PropTypes.string,
+  placeholder: PropTypes.string,
   numberOfLines: PropTypes.number,
   characterLimit: PropTypes.number,
   classes: PropTypes.object.isRequired,
