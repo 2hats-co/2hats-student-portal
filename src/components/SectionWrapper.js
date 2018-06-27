7333cdac35682e78787ae7b82f7d576c79623e23
shams mosowi <shams.mosowi@gmail.com>
2018-06-27 14:09:06 +1000

    section wrapper used in resume builder

---
+++ src/components/SectionWrapper.js
@@ -0,0 +1,33 @@
+//props: title?, child/childern, hint?, characterCounter?
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Typography, Grid } from '@material-ui/core';
+
+
+
+const styles = theme => ({
+
+ 
+});
+
+function SectionWrapper(props) {
+  const { classes, width,height, child } = props;
+ 
+     
+  return (
+   <div style= {{margin:0,width:width,height:height}}>
+     {child}
+  </div>
+  );
+}
+
+SectionWrapper.propTypes = {
+  height: PropTypes.number.isRequired,
+  width: PropTypes.number.isRequired,
+  child: PropTypes.element.isRequired,
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SectionWrapper);
