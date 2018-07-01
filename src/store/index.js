c27f1759171fa84b891d12e12e3e61e7d06981da
shams mosowi <shams.mosowi@gmail.com>
2018-07-01 13:32:20 +1000

    redux setup

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore,applyMiddleware } from 'redux';
+import {createLogger} from 'redux-logger'
+import rootReducer from '../reducers';
+
+const store = createStore(
+    rootReducer,
+    undefined,
+    applyMiddleware(logger)
+);
+
+export default store;
