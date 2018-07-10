2fbb16f1ebe99793bd00980f4c4259d9e7cf5a68
shams mosowi <shams.mosowi@gmail.com>
2018-07-10 10:40:33 +1000

    authenication update

---
+++ src/utilities/Session/withAuthentication.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { auth } from '../../store';
+
+const withAuthentication = (Component) => {
+  class WithAuthentication extends React.Component {
+    componentDidMount() {
+      const { onSetAuthUser } = this.props;
+
+      auth.onAuthStateChanged(authUser => {
+        authUser
+          ? onSetAuthUser(authUser)
+          : onSetAuthUser(null);
+      });
+    }
+
+    render() {
+      return (
+        <Component />
+      );
+    }
+  }
+
+  const mapDispatchToProps = (dispatch) => ({
+    onSetAuthUser: (authUser) => dispatch({ type: 'AUTH_USER_SET', authUser }),
+  });
+
+  return connect(null, mapDispatchToProps)(WithAuthentication);
+}
+
+export default withAuthentication;
