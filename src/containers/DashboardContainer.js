5f581afd0b1c83a5c29cedd443ddd042b584dab0
shams mosowi <shams.mosowi@gmail.com>
2018-07-14 17:30:15 +1000

    dashboard appilcation proccess stepper

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
 import DashboardWrapper from '../components/DashboardWrapper';
 import { Typography } from '@material-ui/core';
-import { compose } from 'recompose';
-import withAuthorisation from '../utilities/Session/withAuthorisation'
 
+import { withStyles } from '@material-ui/core/styles';
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+const styles = theme => ({
+    root: {
+     
+    }
+});
 class DashboardContainer extends Component{
     
     render(){
+        const {classes} = this.props
+        const steps =['Resume Submission','Online Interview','Assessment Centre','Job Placement']
         return(
             <DashboardWrapper header='Dashboard'>
             <Typography variant='display1'>
             Application Timeline
+            <Stepper
+              className={classes.stepper}
+              activeStep={0}
+              alternativeLabel
+            >
+              {steps.map(label => {
+                return (
+                  <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                  </Step>
+                );
+              })}
+            </Stepper>
             </Typography>
             </DashboardWrapper>
         )
@@ -18,6 +40,4 @@ class DashboardContainer extends Component{
 
 }
 
-const authCondition = (authUser) => !!authUser;
-
-export default compose(withAuthorisation(authCondition)(DashboardContainer))
+export default withStyles(styles)(DashboardContainer);
