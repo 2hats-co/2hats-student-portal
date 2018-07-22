f6410be34d3a55f30acf68133b716c7232464682
shams mosowi <shams.mosowi@gmail.com>
2018-07-22 14:08:48 +1000

    loading upcoming events

--- src/components/Dashboard/ListItem.js
+++ src/components/Dashboard/ListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography'
 import { withStyles } from "@material-ui/core/styles";
 import Grid  from "@material-ui/core/Grid";
+import IconButton  from "@material-ui/core/IconButton";
 
 const styles = theme => ({
     root: {
@@ -19,30 +20,40 @@ const styles = theme => ({
         height:65,
         width:65
     },
+    link:{
+        color:'#000',
+        'a:link':{color:'#000'},
+        'a:visited':{color:'#000'},
+        'a:hover':{color:'#000'},
+        'a:active':{color:'#000'}
+    }
   });
 function ListItem(props){
-    const {classes,actionIcon} = props
-   
+    const {classes,actionIcon,month,date,title,body,link} = props
+     console.log(link)
     return(
     <Grid container direction='row' className={classes.root} alignItems='center'>
     <Grid container direction='column'className={classes.date} alignItems='center' justify='center'>
     <Typography variant='button'>
-        JUN
+        {month}
     </Typography>
     <Typography variant='display1'>
-         10
+        {date}
     </Typography>
     </Grid>
     <Grid container direction='column'className={classes.content} justify='center'>
     <Typography variant='title'>
-        Event Name
+        {title}
     </Typography>
     <Typography variant='body1'>
-         event location
+         {body}
     </Typography>
     </Grid>
     <Grid container className={classes.action} alignItems='center' justify='center' >
-        {actionIcon}
+    <IconButton className={classes.button} component="span"><a className={classes.link} href={link} target="_blank">
+             {actionIcon}</a>
+        </IconButton>
+        
     </Grid>
     </Grid>
    )
--- src/components/Dashboard/UpcomingEvents.js
+++ src/components/Dashboard/UpcomingEvents.js
@@ -5,6 +5,7 @@ import ListItem from './ListItem'
 import LinkIcon from '@material-ui/icons/KeyboardArrowRight'
 import Divider from '@material-ui/core/Divider'
 import * as _ from 'lodash'
+import zIndex from '../../../node_modules/@material-ui/core/styles/zIndex';
 const styles = theme => ({
     root: {
      width:500,
@@ -19,15 +20,25 @@ function getTimeStampData(timestamp){
  return {date,month,hour,minutes}
 }
 function UpcomingEvents(props){
+  let items = []
     const {classes,data} = props
-    _.forEach(data,event=>{
-      console.log('upcomingEvents',getTimeStampData(event.startDate))
+    const orderedData = _.sortBy(data,['startDate'])
+    _.forEach(orderedData,(event,key)=>{
+    
+      const date = getTimeStampData(event.startDate).date
+      const month = getTimeStampData(event.startDate).month
+      const title = event.name
+      const body = event.location
+      const link = event.url
+     items.push({key,date,month,title,body,link})
     })
     return(<div className={classes.root}><Typography variant='display1'>
      Upcoming Events
     </Typography>
     <Divider/>
-    <ListItem actionIcon={<LinkIcon style={{fontSize:38}}/>}/>
+    {items.map(x=> <ListItem key={x.key} title={x.title} date={x.date} month={x.month} body={x.body} link={x.link} actionIcon={<LinkIcon style={{fontSize:38}}/>}/>)}
+  
+    
     <Divider/>
    </div>)
 }
