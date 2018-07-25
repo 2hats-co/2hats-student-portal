80597f36a0629bcefb79a3be9f01fb798f1d59c2
Ke Zhang <zhangke0805@gmail.com>
2018-07-25 17:23:23 +1000

    auth process issue fix

--- src/store/index.js
+++ src/store/index.js
@@ -6,26 +6,34 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 import logger from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-import { productionConfig,developmentConfig } from '../config/firebase'
+import { productionConfig, developmentConfig } from '../config/firebase'
 
 import rootReducer from '../reducers'
 
 if (process.env.NODE_ENV === 'production') {
-    firebase.initializeApp(productionConfig)
+	firebase.initializeApp(productionConfig)
 } else {
-    firebase.initializeApp(developmentConfig)
+	firebase.initializeApp(developmentConfig)
 }
 
 export function configureStore(initialState, history) {
-  	const enhancers = []
-  	// Provide timestamp settings to silence warning about deprecation
-  	firebase.firestore().settings({ timestampsInSnapshots: true })
-  	// Dev tools store enhancer
-  	const devToolsExtension = window.devToolsExtension;
-  	if (typeof devToolsExtension === 'function') {
-    	enhancers.push(devToolsExtension());
-  	}
+	const enhancers = []
+	// Provide timestamp settings to silence warning about deprecation
+	firebase.firestore().settings({ timestampsInSnapshots: true })
+	// Dev tools store enhancer
+	const devToolsExtension = window.devToolsExtension;
+	if (typeof devToolsExtension === 'function') {
+		enhancers.push(devToolsExtension());
+	}
+
+	const persistConfig = {
+		key: 'root',
+		storage,
+	}
+	const persistedReducer = persistReducer(persistConfig, rootReducer)
 	const createStoreWithMiddleware = compose(
 		// Add redux firestore store enhancer
 		reduxFirestore(firebase),
@@ -33,10 +41,10 @@ export function configureStore(initialState, history) {
 		...enhancers
 	)(createStore)
 
-	const store = createStoreWithMiddleware(rootReducer);
+	const store = createStoreWithMiddleware(persistedReducer);
 
-  	return store;
+	return store;
 }
 export const auth = firebase.auth();
-export const storage = firebase.storage().ref();
+export const firebaseStorage = firebase.storage().ref();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
