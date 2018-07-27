3ff7a2cad7791f50362cb54d5a4f53046c9c934c
Yi Chin <yi@2hats.com.au>
2018-07-27 16:17:12 +1000

    Add static layout for email verification page.

--- src/containers/EmailVerificationContainer.js
+++ src/containers/EmailVerificationContainer.js
@@ -1,171 +1,100 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import LogoOnCard from '../components/LogoOnCard';
+import LogoInCard from '../components/LogoInCard';
 import OpenMail from '../assets/images/graphics/EmailVerification.png'
 import { Grid, Button, Typography, Dialog, DialogContent, DialogActions, DialogTitle, DialogContentText, Slide, TextField } from '@material-ui/core';
 import SectionWrapper from '../components/SectionWrapper';
 import { validateEmail } from '../utilities/validators';
 
-function Transition(props) {
-    return <Slide direction="up" {...props} />;
-}
+// Redux
+import { compose } from 'redux';
+import { withHandlers } from 'recompose'
+import { connect } from 'react-redux';
+import  {withFirestore} from '../utilities/withFirestore';
+
+// Routing
+import {withRouter} from 'react-router-dom'
+import { COLLECTIONS } from "../constants/firestore";
+
 const styles = theme => ({
-    grid: {
-        height: 460
-    },
-    footerButtons: {
-        width: 440,
+    root: {
+        flexGrow: 1,
+        'padding-top': '0px',
+        'padding-right': '40px',
+        'padding-bottom': '40px',
+        'padding-left': '40px'
     },
     button: {
-        width: 200,
-    },
-    image: {
-        width: 200,
-    },
-    textField: {
-        //marginTop:-12,
-        width: '100%'
-      },
+        'text-align': 'center'
+    }
 });
 
-const body = ['To ensure you can successfully receive our feedback, we have sent a verification email to your provided email address. ',
-    'Please click on the link in the email to verify your email address. You will be directed to our dashboard once your email is verified. ',
-    'If you have not received such email from 2hats, you can request another verification email or modify your email address. ',
-]
 class EmailVerificationContainer extends React.Component {
-    state = {
-        modifyDialog: false,
-        resendDialog: false,
-    };
-    handleChangeEmail = () => {
-        this.setState({
-            modifyDialog: true,
-        });
+    componentWillMount() {
+        if(this.props.history.location.search) {
+            const url = this.props.history.location.search;
+            const evKeyName = "?evKey=";
+            
+            if(url.indexOf(evKeyName)!== -1) {
+                const evKeyVal = url.slice(evKeyName.length, url.length);
+                if(evKeyVal !== "") {
+                    this.props.onEmailVerificationsUpdate(evKeyVal);
+                }
+            }
+        }
     }
-    handleSendVerificationEmail = () => {
-        this.setState({
-            resendDialog: true
-        });
-    }
-    handleClose = () => {
-        this.setState({
-            modifyDialog: false,
-            resendDialog: false
-        });
-    };
-    handleChange = name => event => {
-        this.setState({
-          [name]: event.target.value,
-        });
-      };
+    
     render() {
         const { classes } = this.props;
-        const modifyEmailDialog = (<Dialog
-            open={this.state.modifyDialog}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={this.handleClose}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
-        >
-            <DialogTitle id="alert-dialog-slide-title">
-            <Typography variant='title' color='primary'>
-                Modify email
-                </Typography>
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                Please enter the correct email address below. 
-              </DialogContentText>
-                <TextField
-                    id="email"
-                    label="Email Address"
-                    placeholder="Email Address"
-                    onChange={this.handleChange('email')}
-                    className={classes.textField}
-                    margin="normal"
-                    color="primary"
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={this.handleClose} disabled={!validateEmail(this.state.email)} color="primary">
-                    Modify
-              </Button>
-            </DialogActions>
-        </Dialog>)
-        const resendVerifiactionEmailDialog = (<Dialog
-            open={this.state.resendDialog}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={this.handleClose}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
-        >
-            <DialogTitle id="alert-dialog-slide-title">
-            <Typography variant='title' color='primary'>
-                Resend email
-            </Typography>
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    We have just resent the verification email. Please check your inbox and follow the instruction in the email.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                    Okay
-              </Button>
-            </DialogActions>
-        </Dialog>)
+     
         return (
-            <LogoOnCard width={560}>
-                <SectionWrapper SectionWrapper height={600} child={(<Grid container
-                    className={classes.grid}
-                    direction="column"
-                    justify='space-between'
-                    alignItems='center'
-                >
-                    <Typography variant="display1">
-                        Your submission is received!
-                      </Typography>
-
-                    <img alt='email graphic' className={classes.image} src={OpenMail} />
-                    {body.map((p) => <Typography key={p} variant="body1">
-                        {p}
-                    </Typography>)}
-                    <Grid container
-                        direction="row"
-                        className={classes.footerButtons}
-                        justify='space-between'
-                    >
-                        <Button variant="outlined"
-                            className={classes.button}
-                            color="primary"
-                            onClick={this.handleChangeEmail}
-                        >
-                            Modify email
-                </Button>
-                        <Button
-                            className={classes.button}
-                            variant="flat"
-                            color="primary"
-                            onClick={this.handleSendVerificationEmail}>
-                            Resend Email
-                </Button>
+            <LogoInCard height={350}>
+                <div className={classes.root}>
+                    <Grid container spacing={24}>
+                        <Grid item xs={12}>
+                            <Typography variant="title" color="primary">
+                                Account Validated
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Typography variant="Body 1">
+                                Your 2hats account has been validated. You can jump back to your application process now.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}  className={classes.button} >
+                            <Button
+                                variant="flat"
+                                color="primary"
+                            >
+                                Continue My Application
+                            </Button>
+                        </Grid>
                     </Grid>
-                </Grid>)}/>
-                
-                {resendVerifiactionEmailDialog}
-                {modifyEmailDialog}
-            </LogoOnCard>
-
+                </div>
+            </LogoInCard>
         )
     }
-
-
 }
+
 EmailVerificationContainer.propTypes = {
     classes: PropTypes.object.isRequired,
 };
-export default withStyles(styles)(EmailVerificationContainer);
+
+const enhance = compose(
+    // add redux store (from react context) as a prop
+    withFirestore,
+    // Handler functions as props
+    withHandlers({
+        onEmailVerificationsUpdate: props => (evKey) =>
+            props.firestore.update({ collection: COLLECTIONS.emailVerifications, doc: evKey }, {
+                emailVerified: true,
+                updatedAt: props.firestore.FieldValue.serverTimestamp()
+            }
+        )
+    })
+)
+  
+export default enhance(
+    withRouter(withStyles(styles)(EmailVerificationContainer))
+)
