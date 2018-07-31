89e96e14d9365c079d9d193246aac3a8aebdefec
Yi Chin <yi@2hats.com.au>
2018-07-31 15:10:19 +1000

    Present button based on user login state and profile completed state.

--- src/containers/EmailVerificationContainer.js
+++ src/containers/EmailVerificationContainer.js
@@ -2,19 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import LogoInCard from '../components/LogoInCard';
-import { Grid, Button, Typography, Dialog, DialogContent, DialogActions, DialogTitle, DialogContentText, Slide, TextField } from '@material-ui/core';
-import SectionWrapper from '../components/SectionWrapper';
-import { validateEmail } from '../utilities/validators';
-import { isLoaded, isEmpty } from 'react-redux-firebase';
+import { Grid, Button, Typography } from '@material-ui/core';
+import { PROCESS_TYPES } from '../constants/signUpProcess'
 
 // Redux
 import { compose } from 'redux';
-import { withHandlers, lifecycle } from 'recompose';
 import { connect } from 'react-redux';
-import  {withFirestore} from '../utilities/withFirestore';
 
 // Routing
 import {withRouter} from 'react-router-dom'
+import * as routes from '../constants/routes'
 import { COLLECTIONS, LISTENER } from "../constants/firestore";
 
 const styles = theme => ({
@@ -80,12 +77,48 @@ class EmailVerificationContainer extends React.Component {
             }
         }
     }
+
+    handleSigninClick = () => {
+        this.props.history.replace(routes.SIGN_IN);
+    }
+
+    handleContinueClick = () => {
+        const { authUser, profiles } = this.props;
+        let route;
+
+        if(profiles[authUser.uid].process === PROCESS_TYPES.upload){
+            route = routes.UPLOAD_RESUME;
+        } else {
+            route = routes.BUILD_RESUME;
+        }
+        this.props.history.replace(route);
+    }
+
+    handleProfileClick = () => {
+        this.props.history.replace(routes.PROFILE);
+    }
+
+    handleDashboardClick = () => {
+        this.props.history.replace(routes.DASHBOARD);
+    }
     
     render() {
-        const { classes, profiles } = this.props;
+        const { classes, authUser, profiles } = this.props;
+        let button;
 
-        if(!isLoaded(profiles) || isEmpty(profiles)) {
-            return (null);
+        if(!authUser) {
+            button = <Button variant="flat" color="primary" onClick={this.handleSigninClick}>Sign in</Button>;
+        } else if(authUser.uid && profiles && profiles[authUser.uid]) {
+            const user = profiles[authUser.uid];
+            const { isComplete, hasSubmit } = user;
+
+            if(!isComplete) {
+                button = <Button variant="flat" color="primary" onClick={this.handleContinueClick}>Continue</Button>;
+            } else if(!hasSubmit) {
+                button = <Button variant="flat" color="primary" onClick={this.handleProfileClick}>Go to profile</Button>;
+            } else {
+                button = <Button variant="flat" color="primary" onClick={this.handleDashboardClick}>Go to dashboard</Button>;
+            }
         }
      
         return (
@@ -103,12 +136,7 @@ class EmailVerificationContainer extends React.Component {
                             </Typography>
                         </Grid>
                         <Grid item xs={12}  className={classes.button} >
-                            <Button
-                                variant="flat"
-                                color="primary"
-                            >
-                                Continue My Application
-                            </Button>
+                            {button}
                         </Grid>
                     </Grid>
                 </div>
@@ -119,6 +147,9 @@ class EmailVerificationContainer extends React.Component {
 
 EmailVerificationContainer.propTypes = {
     classes: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
+    authUser: PropTypes.object.isRequired,
+    profiles: PropTypes.object.isRequired,
 };
 
 function mapStateToProps(state){
