37ce8245795a1bf4501feae9be5f51f30e6f475f
shams mosowi <shams.mosowi@gmail.com>
2018-08-03 12:47:04 +1000

    renaming res status

--- src/components/InputFields/WorkingRights.js
+++ src/components/InputFields/WorkingRights.js
@@ -5,12 +5,12 @@ function WorkingRights(props){
     return(
         <DropDown
         hasLabel={hasLabel}
-        title={"Residency Status"}
-        label={"Residency Status"}
+        title={"Work Condition"}
+        label={"Work Condition"}
         name="workingRights"
         value={value}
         changeHandler={changeHandler}
-        list={["Unrestricted - full working rights in Australia", "Restricted - 40 hours per fortnight"]}
+        list={["Unrestricted - full working rights in Australia", "Restricted - up to 40 hours per fortnight"]}
         hint={"We accept candidates with both restricted and unrestricted working rights. Your answer will not impact your submission."}
       />
     )
