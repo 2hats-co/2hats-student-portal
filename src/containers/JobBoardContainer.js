7594ad7c63c90932bfc4b34d37b03979739236be
shams mosowi <shams.mosowi@gmail.com>
2018-08-07 18:25:22 +1000

    dialog title

--- src/containers/JobBoardContainer.js
+++ src/containers/JobBoardContainer.js
@@ -42,7 +42,7 @@ class JobBoardContainer extends React.Component {
           aria-labelledby="alert-dialog-title"
           aria-describedby="alert-dialog-description"
         >
-          <DialogTitle id="alert-dialog-title">{"2hats Talent Pool Only"}</DialogTitle>
+          <DialogTitle style={{paddingLeft:40}} id="alert-dialog-title" >{"2hats Talent Pool Only"}</DialogTitle>
           <DialogContent className={classes.content}>
             <Typography variant='body1'>
 Once you pass the 2hats application process, you will be accepted into the 2hats talent pool, and will gain access to this Job Board. 
@@ -51,7 +51,7 @@ In the meantime, you can check our dashboard for resume feedback and any upcomin
             </Typography>
           </DialogContent>
           <DialogActions>
-            <Button className={classes.button} variant='contained' onClick={this.handleClose} autoFocus>
+            <Button className={classes.button} id={`job-board-back-button`} variant='contained' onClick={this.handleClose} autoFocus>
             Go to Dashboard
             </Button>
           </DialogActions>
