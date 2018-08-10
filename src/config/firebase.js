d08b5446b9aff3d91fb9ae0bd40017de479aeb1c
Yi Chin <yi@2hats.com.au>
2018-08-10 10:47:57 +1000

    Fix staging student portal connects to production server problem.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,29 +3,21 @@ const PRODUCTION_PROJECT_NAME = 'production2hats'
 
 const staging2hatsKey = "AIzaSyC5X6WfsorYvEG_wZacfhg7Y6QP4IgJ9DI"
 const production2hatsKey = "AIzaSyD9EnwYfFxTvnaDMA7r6MbKoXmZbQmukrg"
-export const stagingConfig = {
-    apiKey: staging2hatsKey,
-    authDomain: `${STAGING_PROJECT_NAME}.firebaseapp.com`,
-    databaseURL: `https://${STAGING_PROJECT_NAME}.firebaseio.com`,
-    projectId: STAGING_PROJECT_NAME,
-    storageBucket: `${STAGING_PROJECT_NAME}.appspot.com`,
-    messagingSenderId: "188089188588"
-  };
 
-// export const productionConfig = {
-//     apiKey: "AIzaSyAao6BwLXZzmhzLSGSypBFYw-zAM9wbI44",
-//     authDomain: "hatstest-860eb.firebaseapp.com",
-//     databaseURL: "https://hatstest-860eb.firebaseio.com",
-//     projectId: "hatstest-860eb",
-//     storageBucket: "hatstest-860eb.appspot.com",
-//     messagingSenderId: "839771603283"
-//   };
+export const stagingConfig = {
+  apiKey: staging2hatsKey,
+  authDomain: `${STAGING_PROJECT_NAME}.firebaseapp.com`,
+  databaseURL: `https://${STAGING_PROJECT_NAME}.firebaseio.com`,
+  projectId: STAGING_PROJECT_NAME,
+  storageBucket: `${STAGING_PROJECT_NAME}.appspot.com`,
+  messagingSenderId: "188089188588"
+};
 
 export const productionConfig = {
-    apiKey: production2hatsKey,
-    authDomain: `${PRODUCTION_PROJECT_NAME}.firebaseapp.com`,
-    databaseURL: `https://${PRODUCTION_PROJECT_NAME}.firebaseio.com`,
-    projectId: PRODUCTION_PROJECT_NAME,
-    storageBucket: `${PRODUCTION_PROJECT_NAME}.appspot.com`,
-    messagingSenderId: "188089188588"
-  };
+  apiKey: production2hatsKey,
+  authDomain: `${PRODUCTION_PROJECT_NAME}.firebaseapp.com`,
+  databaseURL: `https://${PRODUCTION_PROJECT_NAME}.firebaseio.com`,
+  projectId: PRODUCTION_PROJECT_NAME,
+  storageBucket: `${PRODUCTION_PROJECT_NAME}.appspot.com`,
+  messagingSenderId: "188089188588"
+};
