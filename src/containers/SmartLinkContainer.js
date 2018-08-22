2ac1a9fbc62a0484874f8be6691263775741e82c
Yi Chin <yi@2hats.com.au>
2018-08-22 14:38:28 +1000

    Fix smart link container not sending smart link call error.

--- src/containers/SmartLinkContainer.js
+++ src/containers/SmartLinkContainer.js
@@ -6,53 +6,43 @@ import { Typography } from "@material-ui/core";
 import { auth } from "../store";
 import { CLOUD_FUNCTIONS, cloudFunction } from '../utilities/CloudFunctions';
 
-class SmartLinkContainer extends React.Component {
-  componentWillMount() {
-    const queryStr = this.props.history.location.search;
+function SmartLinkContainer(props) {
+	const queryStr = props.history.location.search;
 
-    if (queryStr) {
-      const slKeyName = "?slKey=";
+	if (queryStr) {
+		const slKeyName = "?slKey=";
 
-      if (queryStr.indexOf(slKeyName) !== -1) {
-        const slKeyVal = queryStr.slice(slKeyName.length, queryStr.length);
+		if (queryStr.indexOf(slKeyName) !== -1) {
+			const slKey = queryStr.slice(slKeyName.length, queryStr.length);
 
-        if (slKeyVal !== "") {
-          this.setState({ slKey: slKeyVal });
-        }
-      }
-    }
-  }
-  componentDidUpdate(prevProps, prevState){
-    if(prevState.slKey !== this.state.slKey){
-      const { slKey } = this.state;
-      const request = {
-        slKey: slKey
-      };
+			if (slKey !== "") {
+				const request = {
+					slKey: slKey
+				};
 
-      cloudFunction(CLOUD_FUNCTIONS.SMART_LINK, request,
-        (result) => {
-          // Sign in user with custom token.
-          auth.signInWithCustomToken(result.data.token).then(authUser => {
-            // Redirect page based on the route.
-            const route = result.data.route;
-            const firstName = authUser.user.displayName.split(" ")[[0]];
-            this.props.history.replace(route + `?firstName=${firstName}`);
-          });
-        },
-        (error) => {
-          // Getting the Error details.
-          console.log("Call smartLink error: ", error.message);
-        });
-    }
-  }
+				cloudFunction(CLOUD_FUNCTIONS.SMART_LINK, request,
+					(result) => {
+						// Sign in user with custom token.
+						auth.signInWithCustomToken(result.data.token).then(authUser => {
+							// Redirect page based on the route.
+							const route = result.data.route;
+							const firstName = authUser.user.displayName.split(" ")[[0]];
+							props.history.replace(route + `?firstName=${firstName}`);
+						});
+					},
+					(error) => {
+						// Getting the Error details.
+						console.log("Call smartLink error: ", error.message);
+					});
+			}
+		}
+	}
 
-  render() { 
-    return(
-      <LogoInCard isLoading={true}>
-        <Typography variant='title' style={{width:'100%',textAlign:'center'}}>Please hold as we redirect you</Typography>
-      </LogoInCard>
-    );
-  }
+	return (
+		<LogoInCard isLoading={true}>
+			<Typography variant='title' style={{ width: '100%', textAlign: 'center' }}>Please hold as we redirect you</Typography>
+		</LogoInCard>
+	);
 }
 
 export default withRouter(SmartLinkContainer);
