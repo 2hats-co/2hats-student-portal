dc7fefda20e3016a9337ea0bcd76056f8166f708
shams mosowi <shams.mosowi@gmail.com>
2018-09-08 14:42:04 +1000

    padding

--- src/components/Dashboard/ApplicationTimeLine.js
+++ src/components/Dashboard/ApplicationTimeLine.js
@@ -4,20 +4,19 @@ import Stepper from '@material-ui/core/Stepper'
 import StepLabel from '@material-ui/core/StepLabel'
 import Step from '@material-ui/core/Step'
 import { withStyles } from "@material-ui/core/styles";
+import { Grid } from '@material-ui/core';
 
 const STAGES = ['pre-review','resume','interview','assessment','placed']
 
   const styles = theme => ({
     root: {
+      width:'90%',
       paddingTop: 30,
-      paddingLeft: 30,
-      [theme.breakpoints.up('sm')]: {
-        paddingLeft: 0,
+      [theme.breakpoints.up('xs')]: {
+          paddingLeft: 30
       },
-    },
-    title: {
       [theme.breakpoints.up('sm')]: {
-        paddingLeft: 'calc(50% - 250px)'
+          paddingLeft: 'calc(50% - 250px)'
       },
       [theme.breakpoints.up('md')]: {
           paddingLeft: 100
@@ -25,19 +24,6 @@ const STAGES = ['pre-review','resume','interview','assessment','placed']
     },
     label:{
       fontSize: '13px !important'
-    },stepper:{
-      [theme.breakpoints.up('sm')]: {
-        paddingLeft: 'calc(50% - 250px)'
-      },
-      [theme.breakpoints.up('md')]: {
-          paddingLeft: 70
-      },
-    },
-    stepperMobile: {
-      paddingLeft: 0,
-      [theme.breakpoints.up('sm')]: {
-        paddingLeft: 'calc(50% - 250px)',
-      },
     }
    });
 function ApplicationTimeLine(props){
@@ -50,22 +36,29 @@ function ApplicationTimeLine(props){
     }
     const steps =['Resume Submission','Online Interview','Assessment Centre','Job Placement']
 
-    return(<div className={classes.root}><Typography variant='display1' className={classes.title}>
-    Application Timeline
-    </Typography>
-    <Stepper
-      orientation={theme.responsive.isMobile?"vertical":"horizontal"}
-      className={theme.responsive.isMobile ? classes.stepperMobile : classes.stepper}
-      activeStep={activeStep}
-      alternativeLabel={!theme.responsive.isMobile}
-    >
-      {steps.map(label => {
-        return (
-          <Step key={label}>
-            <StepLabel className={classes.label}>{label}</StepLabel>
-          </Step>
-        );
-      })}
-    </Stepper></div>)
+    return(<Grid className={classes.root} container direction='column' alignItems='flex-start' justify='flex-start'>
+    <Grid item>
+      <Typography variant='display1' className={classes.title}>
+      Application Timeline
+      </Typography>
+    </Grid>
+    <Grid item style={{width:'100%'}}>
+      <Stepper
+        orientation={theme.responsive.isMobile?"vertical":"horizontal"}
+        className={theme.responsive.isMobile ? classes.stepperMobile : classes.stepper}
+        activeStep={activeStep}
+        alternativeLabel={!theme.responsive.isMobile}
+      >
+        {steps.map(label => {
+          return (
+            <Step key={label}>
+              <StepLabel className={classes.label}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+    </Grid>
+   
+    </Grid>)
 }
 export default withStyles(styles,{ withTheme: true })(ApplicationTimeLine);
