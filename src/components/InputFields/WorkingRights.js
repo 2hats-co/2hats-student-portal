20d07954dd6b2692b713f4c36c2961d482964881
shams mosowi <shams.mosowi@gmail.com>
2018-09-10 11:02:26 +1000

    merge working rights rm padding

--- src/components/InputFields/WorkingRights.js
+++ src/components/InputFields/WorkingRights.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import DropDown from "../InputFields/DropDown";
-<<<<<<< HEAD
-function WorkingRights(props){
-    const {value,hasLabel,changeHandler,maxWidth} = props
-    return(
-        <DropDown
-        hasLabel={hasLabel}
-        maxWidth ={maxWidth}
-        title={"Work Condition"}
-        label={"Work Condition"}
-        name="workingRights"
-        value={value}
-        changeHandler={changeHandler}
-        list={["Unrestricted - full working rights in Australia", "Restricted - up to 40 hours per fortnight"]}
-        hint={"We accept candidates with both restricted and unrestricted working rights. Your answer will not impact your submission."}
-        />
-    )
-=======
 
 class WorkingRights extends React.Component{
     handleChange(name,value){
@@ -26,7 +9,6 @@ class WorkingRights extends React.Component{
     render(){
         const {value,hasLabel,maxWidth} = this.props
         return(
-            <div style={{paddingTop:20}}>
                 <DropDown
                 hasLabel={hasLabel}
                 maxWidth ={maxWidth}
@@ -38,10 +20,8 @@ class WorkingRights extends React.Component{
                 list={["Unrestricted - full working rights in Australia", "Restricted - up to 40 hours per fortnight"]}
                 hint={"We accept candidates with both restricted and unrestricted working rights. Your answer will not impact your submission."}
                 />
-            </div>
         )
     }
->>>>>>> 2164f0296a65d7115c443a65a1b2d2a44f934e1c
 }
 
 export default WorkingRights
