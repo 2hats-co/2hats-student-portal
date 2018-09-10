66f66b11257614f262e3f73ad57adabcf1437e75
Sidney Alcantara <theisidney@gmail.com>
2018-09-10 16:57:09 +1000

    offline

--- src/components/LogoOnCard.js
+++ src/components/LogoOnCard.js
@@ -9,6 +9,8 @@ import {setBackground} from '../utilities/styling'
 import Tween from 'rc-tween-one';
 import { Grid } from '@material-ui/core';
 
+import Background from '../assets/background/BW.svg';
+
 import { compose } from 'recompose';
 import withAuthorisation from '../utilities/Session/withAuthorisation'
 import SupportFooter from './SupportFooter';
@@ -49,7 +51,8 @@ function LogoOnCard(props) {
   }else{
     const {classes,height,startingWidth,animateWidth,animateHeight,width,theme } = props;
     const {isMobile} = theme.responsive
-    setBackground("#E1E1E1",'https://firebasestorage.googleapis.com/v0/b/hatstest-860eb.appspot.com/o/public%2FBW.svg?alt=media&token=596de8ea-53d1-4be2-afa8-81055b7a6cad',isMobile)
+    // setBackground("#E1E1E1",'https://firebasestorage.googleapis.com/v0/b/hatstest-860eb.appspot.com/o/public%2FBW.svg?alt=media&token=596de8ea-53d1-4be2-afa8-81055b7a6cad',isMobile)
+    setBackground("#E1E1E1",Background,isMobile)
             let style ={}
             let animation = {}
             if(animateWidth){
