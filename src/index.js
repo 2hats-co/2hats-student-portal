3962909fccfef897cc9f03d4e3c7dc80da7f793a
shams mosowi <shams.mosowi@gmail.com>
2018-09-12 15:05:29 +1000

    fb pixel

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import {configureStore} from './store';
 import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import ReactPixel from 'react-facebook-pixel';
 
+ReactPixel.init('2178522349094498', {}, { debug: true, autoConfig: false });
+ReactPixel.pageView();
+ReactPixel.fbq('track', 'PageView');
 const store = configureStore();
 ReactDOM.render(
 <Provider store={store}>
