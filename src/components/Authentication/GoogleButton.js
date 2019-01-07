f2316aa3f5477b4ba3de70ed9252c32f7192d67b
victorhchan <initialvic13@gmail.com>
2019-01-07 12:45:32 +1100

    auth checking response

--- src/components/Authentication/GoogleButton.js
+++ src/components/Authentication/GoogleButton.js
@@ -1,89 +1,93 @@
-import React,{Component} from 'react';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Button from '@material-ui/core/Button';
-import {GOOGLE_CID_STAGING,GOOGLE_CID_PRODUCTION} from '../../config/auth';
-import GoogleIcon from '../../assets/images/social/google.svg';
-import GoogleLogin from '../../utilities/Authentication/GoogleLogin.js';
-import {getTokenWith3rdParty} from '../../utilities/Authentication/getTokenWith3rdParty'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import { GOOGLE_CID_STAGING, GOOGLE_CID_PRODUCTION } from "../../config/auth";
+import GoogleIcon from "../../assets/images/social/google.svg";
+import GoogleLogin from "../../utilities/Authentication/GoogleLogin.js";
+import { getTokenWith3rdParty } from "../../utilities/Authentication/getTokenWith3rdParty";
 import { withRouter } from "react-router-dom";
 
 const styles = theme => ({
-    root: {
-      paddingLeft: 50,
-      paddingRight: 50,
-      height: 500
-    },
-    socialButton: {
-      margin: 5,
-      width: 250,
-      height: 40,
-      color: '#fff',
-      backgroundColor: '#0077B5',
-      padding: 0,
-    },
-    socialIcon: {
-      marginRight: 16
-    },
-  });
-class GoogleButton extends Component{
-    constructor(props){
-        super(props)
-        this.handleGoogleAuthFail = this.handleGoogleAuthFail.bind(this);
-        this.handleRouting = this.handleRouting.bind(this)
-        this.getToken = this.getToken.bind(this) 
-        this.state = {cid:GOOGLE_CID_STAGING}
-    }
-    
-    componentWillMount(){
-      if (process.env.REACT_APP_ENV === 'PRODUCTION') {
-        this.setState({cid:GOOGLE_CID_PRODUCTION})
-      } else {
-        this.setState({cid:GOOGLE_CID_STAGING})
-      }
-    }
-    handleRouting(route){
-      this.props.changeHandler('isLoading',false)
-      this.props.history.replace(route)
-    }
-    handleGoogleAuthFail = (error) => {
-  
-        console.log('google auth fail', error)
-    }
- 
-    getToken(r){
-      this.props.changeHandler('isLoading',true)
-      let user={};
-      user.email = r.profileObj.email
-      user.firstName = r.profileObj.givenName
-      user.lastName = r.profileObj.familyName
-      user.provider = {service:'google',id:r.profileObj.googleId}
-      user.avatarURL = r.profileObj.photo ||''
-      getTokenWith3rdParty(user,this.handleRouting)
-    }
-    render(){
-        const {classes,action} = this.props
-        const {cid} = this.state
-        return(
-            <GoogleLogin
-          key={`google-button`}
-          clientId={cid}
-          buttonText="Login"
-          onSuccess={this.getToken}
-          onFailure={this.handleGoogleAuthFail}
-          render={renderProps => (
-            <Button variant='flat'
-              key={`google-button`}
-              style={{ backgroundColor: '#E05449' }}
-              onClick={renderProps.onClick}
-              className={classes.socialButton}
-            >
-                <img alt={'google-logo'} src={GoogleIcon} className={classes.socialIcon} />
-                {action||'Sign in'} with Google
-            </Button>
-          )}
-        />
-        )
-    }
+  root: {
+    paddingLeft: 50,
+    paddingRight: 50,
+    height: 500
+  },
+  socialButton: {
+    margin: 5,
+    width: 250,
+    height: 40,
+    color: "#fff",
+    backgroundColor: "#0077B5",
+    padding: 0
+  },
+  socialIcon: {
+    marginRight: 16
+  }
+});
+class GoogleButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleGoogleAuthFail = this.handleGoogleAuthFail.bind(this);
+    this.handleRouting = this.handleRouting.bind(this);
+    this.getToken = this.getToken.bind(this);
+    this.state = { cid: GOOGLE_CID_STAGING };
+  }
 
+  componentWillMount() {
+    if (process.env.REACT_APP_ENV === "PRODUCTION") {
+      this.setState({ cid: GOOGLE_CID_PRODUCTION });
+    } else {
+      this.setState({ cid: GOOGLE_CID_STAGING });
+    }
+  }
+  handleRouting(route) {
+    this.props.changeHandler("isLoading", false);
+    this.props.history.replace(route);
+  }
+  handleGoogleAuthFail = error => {
+    console.log("google auth fail", error);
+  };
+
+  getToken(r) {
+    this.props.changeHandler("isLoading", true);
+    console.log(r); //See whats in the response
+    let user = {};
+    user.email = r.profileObj.email;
+    user.firstName = r.profileObj.givenName;
+    user.lastName = r.profileObj.familyName;
+    user.provider = { service: "google", id: r.profileObj.googleId };
+    user.avatarURL = r.profileObj.photo || "";
+    getTokenWith3rdParty(user, this.handleRouting);
+  }
+  render() {
+    const { classes, action } = this.props;
+    const { cid } = this.state;
+    return (
+      <GoogleLogin
+        key={`google-button`}
+        clientId={cid}
+        buttonText="Login"
+        onSuccess={this.getToken}
+        onFailure={this.handleGoogleAuthFail}
+        render={renderProps => (
+          <Button
+            variant="flat"
+            key={`google-button`}
+            style={{ backgroundColor: "#E05449" }}
+            onClick={renderProps.onClick}
+            className={classes.socialButton}
+          >
+            <img
+              alt={"google-logo"}
+              src={GoogleIcon}
+              className={classes.socialIcon}
+            />
+            {action || "Sign in"} with Google
+          </Button>
+        )}
+      />
+    );
+  }
 }
-export default withRouter(withStyles(styles)(GoogleButton))
+export default withRouter(withStyles(styles)(GoogleButton));
--- src/components/Authentication/LinkedinButton.js
+++ src/components/Authentication/LinkedinButton.js
@@ -1,88 +1,98 @@
-import React,{Component} from 'react'
-import withStyles from '@material-ui/core/styles/withStyles';
-import Button from '@material-ui/core/Button'
-import LinkedinIcon from '../../assets/images/social/linkedin.svg'
-import {LINKEDIN_CID} from '../../config/auth'
-import {getTokenWith3rdParty} from '../../utilities/Authentication/getTokenWith3rdParty'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import LinkedinIcon from "../../assets/images/social/linkedin.svg";
+import { LINKEDIN_CID } from "../../config/auth";
+import { getTokenWith3rdParty } from "../../utilities/Authentication/getTokenWith3rdParty";
 import { withRouter } from "react-router-dom";
 
 const styles = theme => ({
-    root: {
-      paddingLeft: 50,
-      paddingRight: 50,
-      height: 500
-    },
-    socialButton: {
-      margin: 5,
-      width: 250,
-      height: 40,
-      color: '#fff',
-      backgroundColor: '#0077B5',
-      padding: 0,
-    },
-    socialIcon: {
-      marginRight: 16
-    },
-  });
-class LinkedinButton extends Component{
-    constructor(props){
-        super(props)
-        this.handleLinkedInAuth = this.handleLinkedInAuth.bind(this)
-        this.handleRouting = this.handleRouting.bind(this)
-    }
-    handleRouting(route){
-      this.props.changeHandler('isLoading',false)
-      this.props.history.replace(route)
-    }
-    componentWillMount(){
-        this.initializeLinkedin(LINKEDIN_CID);
-    }
-    getToken(r){
-      this.props.changeHandler('isLoading',true)
-      let user={};
-      user.email = r.emailAddress
-      user.firstName = r.firstName
-      user.lastName = r.lastName
-      user.provider = {service:'linkedin',id:r.id}
-      user.bio = r.summary ||''
-      user.avatarURL = ''
-      getTokenWith3rdParty(user,this.handleRouting)
-    }
-   
-    initializeLinkedin = clientId => {
-        ; ((d, s, id) => {
-          const element = d.getElementsByTagName(s)[0]
-          const ljs = element
-          let js = element
-          if (d.getElementById(id)) {return }
-          js = d.createElement(s)
-          js.id = id
-          js.src = `//platform.linkedin.com/in.js`
-          js.text = `api_key: ${clientId}`
-          ljs.parentNode.insertBefore(js, ljs)
-        })(document, 'script', 'linkedin-jssdk')
-      }
-      handleLinkedInAuth = () => {
-        const fields = ":(id,email-address,headline,summary,first-name,last-name,picture-urls::(original))";
-        window.IN.API.Raw(`/people/~${fields}`).result(async r => {
-          this.getToken(r)
-      })
-    }
-    authorize = e => {
-        window.IN.User.authorize(this.handleLinkedInAuth, '')
-      }    
-    render(){
-      const {classes,action} = this.props
-        return(
-            <Button key={`linkedin-button`} variant='flat'
-          onClick={this.authorize}
-          style={{ backgroundColor: '#0077B5' }}
-          className={classes.socialButton}>
-            <img alt={'linkedinLogo'} src={LinkedinIcon} className={classes.socialIcon} />
-            {action||'sign in'} with LinkedIn
-        </Button>
-        )
-    }
+  root: {
+    paddingLeft: 50,
+    paddingRight: 50,
+    height: 500
+  },
+  socialButton: {
+    margin: 5,
+    width: 250,
+    height: 40,
+    color: "#fff",
+    backgroundColor: "#0077B5",
+    padding: 0
+  },
+  socialIcon: {
+    marginRight: 16
+  }
+});
+class LinkedinButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLinkedInAuth = this.handleLinkedInAuth.bind(this);
+    this.handleRouting = this.handleRouting.bind(this);
+  }
+  handleRouting(route) {
+    this.props.changeHandler("isLoading", false);
+    this.props.history.replace(route);
+  }
+  componentWillMount() {
+    this.initializeLinkedin(LINKEDIN_CID);
+  }
+  getToken(r) {
+    this.props.changeHandler("isLoading", true);
+    let user = {};
+    console.log(r); //See whats in response
+    user.email = r.emailAddress;
+    user.firstName = r.firstName;
+    user.lastName = r.lastName;
+    user.provider = { service: "linkedin", id: r.id };
+    user.bio = r.summary || "";
+    user.avatarURL = "";
+    getTokenWith3rdParty(user, this.handleRouting);
+  }
 
+  initializeLinkedin = clientId => {
+    ((d, s, id) => {
+      const element = d.getElementsByTagName(s)[0];
+      const ljs = element;
+      let js = element;
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = `//platform.linkedin.com/in.js`;
+      js.text = `api_key: ${clientId}`;
+      ljs.parentNode.insertBefore(js, ljs);
+    })(document, "script", "linkedin-jssdk");
+  };
+  handleLinkedInAuth = () => {
+    const fields =
+      ":(id,email-address,headline,summary,first-name,last-name,picture-urls::(original))";
+    window.IN.API.Raw(`/people/~${fields}`).result(async r => {
+      this.getToken(r);
+    });
+  };
+  authorize = e => {
+    window.IN.User.authorize(this.handleLinkedInAuth, "");
+  };
+  render() {
+    const { classes, action } = this.props;
+    return (
+      <Button
+        key={`linkedin-button`}
+        variant="flat"
+        onClick={this.authorize}
+        style={{ backgroundColor: "#0077B5" }}
+        className={classes.socialButton}
+      >
+        <img
+          alt={"linkedinLogo"}
+          src={LinkedinIcon}
+          className={classes.socialIcon}
+        />
+        {action || "sign in"} with LinkedIn
+      </Button>
+    );
+  }
 }
-export default withRouter(withStyles(styles)(LinkedinButton))
+export default withRouter(withStyles(styles)(LinkedinButton));
