0437bf8877ec830091cb29e731323b8a5fa9b6c1
Sidney Alcantara <sidney@sidney.me>
2019-02-09 19:04:44 +1100

    add updatedAt on doc create

--- src/utilities/firestore.js
+++ src/utilities/firestore.js
@@ -33,4 +33,5 @@ export const createDoc = (collection, docData) =>
   firestore.collection(collection).add({
     ...docData,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
   });
