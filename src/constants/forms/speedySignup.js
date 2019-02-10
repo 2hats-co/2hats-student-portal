eef673dab17d26fe7808b7f51e16ca3eec95c816
shams mosowi <shams.mosowi@gmail.com>
2019-02-10 20:02:20 +1100

    reorder speedysignup

--- src/constants/forms/speedySignup.js
+++ src/constants/forms/speedySignup.js
@@ -51,6 +51,19 @@ const speedySignupFields = initialData => {
         .required('Required'),
     },
     {
+      type: FIELDS.autocomplete,
+      name: 'interest',
+      label: 'Interest',
+      suggestions: [
+        { value: 'course', label: 'Courses' },
+        { value: 'job', label: 'Jobs' },
+        { value: 'assessments', label: 'Skill Tasks' },
+      ],
+      validation: yup
+        .string(['course', 'job', 'assessments'], 'Not a valid interest')
+        .required('Required'),
+    },
+    {
       type: FIELDS.textField,
       name: 'currentDegree',
       label: 'Current Degree',
@@ -75,19 +88,6 @@ const speedySignupFields = initialData => {
         .required(
           'Please enter a 10-digit Australian mobile number with no spaces'
         ),
-    },
-    {
-      type: FIELDS.autocomplete,
-      name: 'interest',
-      label: 'Interest',
-      suggestions: [
-        { value: 'course', label: 'Courses' },
-        { value: 'job', label: 'Jobs' },
-        { value: 'assessments', label: 'Skill Tasks' },
-      ],
-      validation: yup
-        .string(['course', 'job', 'assessments'], 'Not a valid interest')
-        .required('Required'),
     },
   ];
 };
