9806c3f73bd73597218ed29ad94c9c0c4c378ce6
Sidney Alcantara <sidney@sidney.me>
2019-02-13 12:01:21 +1100

    attempt fix pipelines merge

---
+++ scripts/deployconfig-staging.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+
+console.log('Running staging deploy config script...');
+
+fs.writeFile(
+  './.env',
+  `REACT_APP_ENV='STAGING'
+SKIP_PREFLIGHT_CHECK=true
+`,
+  function(err) {
+    if (err) throw err;
+    console.log('Successfully wrote .env');
+  }
+);
+
+fs.writeFile(
+  './.firebaserc',
+  `{
+    "projects": {
+      "default": "staging2hats"
+    }
+  }
+  `,
+  function(err) {
+    if (err) throw err;
+    console.log('Successfully wrote .firebaserc');
+  }
+);
+
+fs.writeFile(
+  './firebase.json',
+  `{
+    "hosting": {
+      "target": "staging2hats",
+      "public": "build",
+      "ignore": ["firebase.json", "**/.*", "**/node_modules/**"],
+      "rewrites": [
+        {
+          "source": "**",
+          "destination": "/index.html"
+        }
+      ]
+    }
+  }
+  
+  `,
+  function(err) {
+    if (err) throw err;
+    console.log('Successfully wrote .firebase.json');
+  }
+);
