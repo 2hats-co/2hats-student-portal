48ce2fbeac1d700c24a7bc5dbf22eb202356f1bc
Sidney Alcantara <sidney@sidney.me>
2019-02-19 10:22:36 +1100

    remove console logs from Announcement

--- src/components/Announcement/index.js
+++ src/components/Announcement/index.js
@@ -126,8 +126,6 @@ const styles = theme => ({
   buttonWrapper: { textAlign: 'right' },
 });
 
-console.log(COLLECTIONS);
-
 const Announcement = props => {
   const { classes, theme, width, history } = props;
 
@@ -141,8 +139,6 @@ const Announcement = props => {
     sort: { field: 'createdAt', direction: 'desc' },
   });
   const data = announcementsState.documents[0];
-
-  console.log('announcementsState', announcementsState);
 
   const [yourAssessmentState, yourAssessmentDispatch] = useCollection();
   const yourAssessmentDoc = yourAssessmentState.documents[0];
