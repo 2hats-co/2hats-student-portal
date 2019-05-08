8ca59ce6197119810a52719b45f53210432d2aff
Sidney Alcantara <sidney@sidney.me>
2019-05-08 12:58:55 +1000

    what's next style changes + view feedback button

--- src/components/WhatsNext/WhatsNextBadge.js
+++ src/components/WhatsNext/WhatsNextBadge.js
@@ -1,35 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withRouter } from 'react-router-dom';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 
-import PaddedIcon from '../PaddedIcon';
-import BadgeIcon from '@material-ui/icons/NewReleasesOutlined';
+import BadgeIcon from '../../assets/icons/SkillAchieved';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForwardOutlined';
 
 import { getSkillLabel } from '@bit/sidney2hats.2hats.global.common-constants';
+import * as ROUTES from '../../constants/routes';
 
 const WhatsNextBadge = props => {
-  const { classes, badge, isMobile } = props;
+  const { classes, badge, isMobile, history } = props;
 
   if (!badge) return null;
 
   return (
     <>
-      <Grid container direction={isMobile ? 'column' : 'row'}>
-        <Grid item className={classes.iconWrapper}>
-          <PaddedIcon color="primary">
-            <BadgeIcon />
-          </PaddedIcon>
-        </Grid>
+      <Grid container alignItems="flex-end" spacing={24}>
         <Grid item xs>
-          <Typography variant="h6" className={classes.title} gutterBottom>
-            Congratulations!
-          </Typography>
-          <Typography variant="body1" className={classes.description}>
-            You passed the {badge.type} <b>{badge.title}</b>
-            {badge.skillAssociated && (
+          <Grid container direction={isMobile ? 'column' : 'row'}>
+            <Grid item className={classes.iconWrapper}>
+              <BadgeIcon
+                className={classNames(classes.whatsNextIcon, classes.green)}
+              />
+            </Grid>
+            <Grid item xs>
+              <Typography
+                variant="h6"
+                className={classNames(classes.title, classes.green)}
+                gutterBottom
+              >
+                Congratulations!
+              </Typography>
+              <Typography variant="body1" className={classes.description}>
+                You passed the <b>{badge.title}</b>{' '}
+                {badge.type.replace('assessment', 'task')}
+                {/* No longer needed + whatsNextAI callable still uses hardcoded skills */}
+                {/* {badge.skillAssociated && (
               <>
                 {' '}
                 and earned the <b>
@@ -37,10 +49,27 @@ const WhatsNextBadge = props => {
                 </b>{' '}
                 badge
               </>
-            )}
-            .
-          </Typography>
+            )} */}
+                .
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
+
+        {badge.type === 'assessment' && (
+          <Grid item xs={12} sm="auto">
+            <Button
+              color="primary"
+              variant="outlined"
+              onClick={() => {
+                history.push(`${ROUTES.ASSESSMENT}?id=${badge.id}&yours=true`);
+              }}
+            >
+              View Feedback
+              <ArrowForwardIcon />
+            </Button>
+          </Grid>
+        )}
       </Grid>
       <Divider className={classes.divider} />
     </>
@@ -51,6 +80,7 @@ WhatsNextBadge.propTypes = {
   classes: PropTypes.object.isRequired,
   badge: PropTypes.object,
   isMobile: PropTypes.bool,
+  history: PropTypes.object.isRequired,
 };
 
-export default WhatsNextBadge;
+export default withRouter(WhatsNextBadge);
