08328322485d177371b9b083cc4ef94ece1c3871
Sidney Alcantara <sidney@sidney.me>
2019-06-08 13:26:56 +1000

    add react-axe for accessibility auditing

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ console.log(
   new Date(metadata.date).toLocaleString()
 );
 
+if (process.env.NODE_ENV !== 'production') {
+  var axe = require('react-axe');
+  axe(React, ReactDOM, 1000);
+}
+
 ReactPixel.init('2178522349094498', {}, { debug: true, autoConfig: false });
 ReactPixel.pageView();
 
