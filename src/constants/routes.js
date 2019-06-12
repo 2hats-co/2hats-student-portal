0d1f1d55845561fbed11c386e85ce3df4b71dc91
Sidney Alcantara <sidney@sidney.me>
2019-06-12 10:28:29 +1000

    mark DEPRECATED components

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -6,11 +6,11 @@ export const SPEEDY_SIGN_UP = '/speedySignup';
 export const SIGN_IN = '/signin';
 export const RESET_PASSWORD = '/resetPassword';
 export const VALIDATE_EMAIL = '/validateEmail';
-export const EMAIL_VERIFICATION = '/emailVerifications';
+// export const EMAIL_VERIFICATION = '/emailVerifications';
 export const CREATE_PASSWORD = '/createPassword';
 export const NO_PASSWORD = '/noPassword';
 export const SMART_LINK = '/smartLinks';
-export const PREVIOUS_SUBMISSION = '/prevSubmission';
+// export const PREVIOUS_SUBMISSION = '/prevSubmission';
 
 // SP3 MAIN NAV ITEMS
 export const DASHBOARD = '/dashboard';
