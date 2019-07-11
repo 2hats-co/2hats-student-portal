cfb0575a1ed0f525e373d44e247bb68082e13da7
Sidney Alcantara <sidney@sidney.me>
2019-07-11 17:40:30 +1000

    convert DetailedViewContainer to TS, add comments

--- src/containers/DetailedViewContainer.md
+++ src/containers/DetailedViewContainer.md
@@ -1,2 +1,6 @@
 Based on the route, renders either [`Job`](#job) or [`Assessment`](#assessment).
 If job or assessment ID not found, renders [`FourOhFour`](#fourohfour).
+
+### Switchover to new route format: `/type/:id`
+
+This container will redirect any /type?id=<id> routes to /type/:id format.
--- src/containers/DetailedViewContainer.tsx
+++ src/containers/DetailedViewContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext } from 'react';
-import PropTypes from 'prop-types';
+import { RouteComponentProps } from 'react-router-dom';
 import queryString from 'query-string';
 
 import LoadingScreen from '../components/LoadingScreen';
@@ -7,62 +7,70 @@ import Job from '../components/Job';
 import Assessment from '../components/Assessment';
 import FourOhFour from '../components/routing/FourOhFour';
 
-// import { COLLECTIONS } from '@bit/sidney2hats.2hats.global.common-constants';
 import useDocumentFromUrl from '../hooks/useDocumentFromUrl';
 import { capitalise } from '../utilities';
 import UserContext from 'contexts/UserContext';
 
-const DetailedViewContainer = props => {
-  const { location, history, match } = props;
+export interface DetailedViewContainerProps
+  extends RouteComponentProps<{ id: string }> {}
 
+const DetailedViewContainer: React.FC<DetailedViewContainerProps> = ({
+  match,
+  location,
+  history,
+}) => {
   const { user } = useContext(UserContext);
-
   const docType = location.pathname.split('/')[1];
+
   const [docState] = useDocumentFromUrl(location, match, `${docType}s`);
 
   useEffect(() => {
     document.title = `2hats – ${capitalise(docType)}s`;
 
+    // If not in the /type/:id format, check if it's
+    // in the old /type?id=<id> format first
     if (!match.params || !match.params.id) {
       const parsedQuery = queryString.parse(location.search);
+      // Redirect to new URL format
       if (parsedQuery.id) {
+        const newParsedQuery = { ...parsedQuery };
         const docId = parsedQuery.id;
-        delete parsedQuery.id;
+        delete newParsedQuery.id;
         history.replace(
-          `${location.pathname}/${docId}?${queryString.stringify(parsedQuery)}`
+          `${location.pathname}/${docId}?${queryString.stringify(
+            newParsedQuery
+          )}`
         );
-      } else history.replace(`/${docType}s${location.search}`);
+      } // Otherwise, go to the base cards route
+      else history.replace(`/${docType}s${location.search}`);
     }
   }, [location, match]);
 
+  // Scroll to top and change window title on load
   useEffect(() => {
-    // window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     if (docState.doc)
-      document.title = `2hats – ${capitalise(docType)}s – ${
-        docState.doc.title
-      }`;
+      document.title = `${docState.doc.title} – ${capitalise(
+        docType
+      )}s – 2hats`;
   }, [docState.doc]);
 
+  // Render the corresponding components
   if (docState.doc) {
     switch (docType) {
       case 'job':
         return <Job data={docState.doc} user={user} />;
       case 'assessment':
-        return <Assessment data={docState.doc} user={user} />;
+        return <Assessment data={docState.doc} />;
       default:
         return <FourOhFour />;
     }
   }
 
-  if (docState.loading) return <LoadingScreen showNav />;
+  if (docState.loading)
+    return <LoadingScreen contained message={`Loading ${docType}…`} />;
 
   return <FourOhFour />;
-};
-
-DetailedViewContainer.propTypes = {
-  location: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
-  history: PropTypes.object.isRequired,
 };
 
 export default DetailedViewContainer;
