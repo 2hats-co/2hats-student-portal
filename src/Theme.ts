9a5af3ef626a279f9fb9d9ac88e6299029de3f4c
Sidney Alcantara <sidney@sidney.me>
2019-10-19 22:33:32 +1100

    fix Theme for ts

--- src/Theme.ts
+++ src/Theme.ts
@@ -70,22 +70,18 @@ const darkElevation = {
 
 declare module '@material-ui/core/styles/createMuiTheme' {
   interface Theme {
-    assets?: any;
+    assets: any;
+    shadowsLight: string[];
   }
 }
 
 declare module '@material-ui/core/styles/createPalette' {
   interface PaletteColor {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
+    [shade: number]: string;
+  }
+
+  interface TypeBackground {
+    elevation: { [level: number]: string };
   }
 }
 
