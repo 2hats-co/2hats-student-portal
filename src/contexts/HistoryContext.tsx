fb071ee4be5526f6abd17f4f33e081be92a6d033
Sidney Alcantara <sidney@sidney.me>
2019-10-19 22:33:23 +1100

    convert HistoryContext to ts

--- src/contexts/HistoryContext.tsx
+++ src/contexts/HistoryContext.tsx
@@ -1,15 +1,20 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useReducer, createContext, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import React, { createContext, useReducer, useEffect } from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import equals from 'ramda/es/equals';
 
 import { ROUTES_PREVENT_BACK } from 'constants/routes';
 
-export const HistoryContext = createContext();
+export const HistoryContext = createContext<StackType>([]);
 
-const historyReducer = (prevState, action) => {
-  const pushTo = stack => {
+type StackType = RouteComponentProps['location'][];
+
+type ActionType = {
+  action: 'PUSH' | 'POP' | 'REPLACE';
+  location: RouteComponentProps['location'];
+};
+
+const historyReducer = (prevState: StackType, action: ActionType) => {
+  const pushTo = (stack: StackType) => {
     // Don't push if the same
     if (stack.length > 0 && stack[stack.length - 1].key === action.location.key)
       return stack;
@@ -48,6 +53,24 @@ const historyReducer = (prevState, action) => {
   }
 };
 
+const _HistoryProvider: React.FunctionComponent<RouteComponentProps> = ({
+  history,
+  location,
+  children,
+}) => {
+  const [stack, stackDispatch] = useReducer(historyReducer, []);
+
+  useEffect(() => {
+    // push current location on mount
+    stackDispatch({ action: 'PUSH', location });
+    history.listen((location, action) => stackDispatch({ action, location }));
+  }, []);
+
+  return (
+    <HistoryContext.Provider value={stack}>{children}</HistoryContext.Provider>
+  );
+};
+
 /**
  * Wrapper for `HistoryContext.Provider`. Sets listener for `history`: when
  * `location` changes, uses a reducer to PUSH, POP, or REPLACE in the stack.
@@ -63,23 +86,4 @@ const historyReducer = (prevState, action) => {
  * This could be solved by storing the stack in Session Storage if this feature
  * is required in the future.
  */
-export const HistoryProvider = withRouter(({ history, location, children }) => {
-  const [stack, stackDispatch] = useReducer(historyReducer, []);
-
-  useEffect(() => {
-    // push current location on mount
-    stackDispatch({ action: 'PUSH', location });
-    history.listen((location, action) => stackDispatch({ action, location }));
-  }, []);
-
-  return (
-    <HistoryContext.Provider value={stack}>{children}</HistoryContext.Provider>
-  );
-});
-
-HistoryProvider.propTypes = {
-  history: PropTypes.object,
-  location: PropTypes.object,
-  /** Everything inside, from `App` */
-  children: PropTypes.node.isRequired,
-};
+export const HistoryProvider = withRouter(_HistoryProvider);
