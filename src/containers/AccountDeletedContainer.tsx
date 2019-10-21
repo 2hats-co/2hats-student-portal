601fe7f2be3d409f27030d768a5bef713e60a197
Sidney Alcantara <sidney@sidney.me>
2019-10-21 12:46:40 +1100

    add basic AccountDeletedContainer contents

--- src/containers/AccountDeletedContainer.tsx
+++ src/containers/AccountDeletedContainer.tsx
@@ -1,11 +1,68 @@
-import * as React from 'react';
+import React from 'react';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 
-interface IAccountDeletedContainerProps {}
+import {
+  makeStyles,
+  createStyles,
+  Container,
+} from '@material-ui/core';
 
-const AccountDeletedContainer: React.FunctionComponent<
-  IAccountDeletedContainerProps
-> = props => {
-  return <>your account will be delet soon</>;
+import RightButtonLayout from 'components/Profile/RightButtonLayout';
+import GoIcon from '@bit/twohats.common.icons.go';
+
+import { IS_MOBILE_QUERY } from 'constants/layout';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      paddingTop: theme.spacing(4),
+      [IS_MOBILE_QUERY]: { paddingTop: 0 },
+
+      '& section': { marginBottom: theme.spacing(8) },
+    },
+
+    nobr: { whiteSpace: 'nowrap' },
+  })
+);
+
+
+const AccountDeletedContainer: React.FunctionComponent<RouteComponentProps> = ({
+  history,
+}) => {
+  const classes = useStyles();
+
+
+  return (
+    <Container maxWidth="sm" component="main" className={classes.root}>
+      <section>
+        <RightButtonLayout
+          title={
+            <>
+              Sorry to See You Go <span className={classes.nobr}>:(</span>
+            </>
+          }
+          buttonLabel="Save My Account"
+          ButtonProps={{
+            endIcon: <GoIcon animated />,
+          }}
+          description={
+            <>
+              We are in the process of removing your personal information,
+              résumé, and portfolio from our database.
+              <br />
+              <br />
+              You <Countdown userDeleteRequestDoc={userDeleteRequestDoc} />{' '}
+              cancel your account deletion request by clicking “Save My Account”
+              above.
+              <br />
+              <br />
+              We hope to see you again and wish you luck!
+            </>
+          }
+        />
+      </section>
+    </Container>
+  );
 };
 
-export default AccountDeletedContainer;
+export default withRouter(AccountDeletedContainer);
