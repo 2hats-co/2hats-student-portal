fcccf3b1270275c3fbd82c08728cdec50844309d
Sidney Alcantara <sidney@sidney.me>
2019-11-12 16:54:57 +1100

    OnboardingA3: hook up LocationFields to profile doc

--- src/components/Onboarding/OnboardingA3.tsx
+++ src/components/Onboarding/OnboardingA3.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Formik, Field, Form } from 'formik';
+import isEmpty from 'ramda/es/isEmpty';
 
-import { makeStyles, createStyles, Grid, Typography } from '@material-ui/core';
+import {
+  makeStyles,
+  createStyles,
+  Grid,
+  Typography,
+  LinearProgress,
+} from '@material-ui/core';
 
 import Graphic from 'assets/images/graphics/OnboardingA3.svg';
 
@@ -9,20 +16,61 @@ import Autosave from 'components/Form/Autosave';
 import LocationField from 'components/FormikFields/LocationField';
 import OnboardingCta from './OnboardingCta';
 
+import { useUser } from 'contexts/UserContext';
+import { updateDoc } from 'utilities/firestore';
+import { COLLECTIONS } from '@bit/twohats.common.constants';
+
 const useStyles = makeStyles(theme =>
   createStyles({
     center: { textAlign: 'center' },
     img: { margin: theme.spacing(3, 0) },
-    resumeUploader: { marginTop: theme.spacing(3) },
+
+    form: {
+      '& .field-wrapper': { marginBottom: theme.spacing(2) },
+    },
+
+    loadingWrapper: { height: 172 },
+    loadingBar: { width: '100%' },
   })
 );
 
 const OnboardingA3: React.FC = () => {
   const classes = useStyles();
+  const [disableCta, setDisableCta] = useState(true);
 
+  const { UID, profile: _profile } = useUser();
+  // Temporarily cast profile while bit dbTypes are broken
+  // TODO: Remove this workaround
+  const profile = _profile as { [key: string]: any };
+
+  // Handle autosaving the user’s location
   const handleSave = async (debouncedValue: string) => {
+    const values = JSON.parse(debouncedValue);
+
+    // Check if new values are different before writing to Firestore
+    if (
+      JSON.stringify(values.locationWork) !==
+        JSON.stringify(profile.locationWork) ||
+      JSON.stringify(values.locationHome) !==
+        JSON.stringify(profile.locationHome)
+    ) {
+      await updateDoc(COLLECTIONS.profiles, UID!, JSON.parse(debouncedValue));
+      return { success: true };
+    }
+    // Ensure no snackbar is displayed when the component is first rendered
     return null;
   };
+
+  // Update `disableCta` state based on profile
+  useEffect(() => {
+    if (!profile) return;
+
+    // Calculate new state based on data stored in user profile
+    const newDisableCtaState =
+      isEmpty(profile.locationWork) || isEmpty(profile.locationHome);
+    // Update local state if needed—prevents unnecessary re-renders
+    if (disableCta !== newDisableCtaState) setDisableCta(newDisableCtaState);
+  }, [profile, disableCta]);
 
   return (
     <>
@@ -48,24 +96,24 @@ const OnboardingA3: React.FC = () => {
           component="p"
           color="textSecondary"
           align="left"
-          gutterBottom
+          paragraph
         >
           Tell us about the lucky city you want to work in. It helps us show you
           the most relevant positions for your future.
         </Typography>
 
-        <Formik
-          onSubmit={(values, actions) => {
-            // Does nothing, just sets submitting to false again
-            actions.setSubmitting(false);
-          }}
-          // TODO: Hook this up to real profile
-          initialValues={{ locationWork: [], locationHome: '' }}
-          render={({ values }) => {
-            console.log(values);
-
-            return (
-              <Form>
+        {profile ? (
+          <Formik
+            onSubmit={(values, actions) => {
+              // Does nothing, just sets submitting to false again
+              actions.setSubmitting(false);
+            }}
+            initialValues={{
+              locationWork: profile.locationWork || [],
+              locationHome: profile.locationHome || '',
+            }}
+            render={({ values }) => (
+              <Form className={classes.form}>
                 <Autosave
                   valueToDebounce={JSON.stringify(values)}
                   callback={handleSave}
@@ -83,14 +131,22 @@ const OnboardingA3: React.FC = () => {
                   label="I’m Currently In"
                 />
               </Form>
-            );
-          }}
-        />
+            )}
+          />
+        ) : (
+          <Grid
+            container
+            alignItems="center"
+            className={classes.loadingWrapper}
+          >
+            <LinearProgress className={classes.loadingBar} />
+          </Grid>
+        )}
       </Grid>
 
       <Grid item className={classes.center}>
         <OnboardingCta action="I’ll Decide Later" secondary />
-        <OnboardingCta action="Continue" />
+        <OnboardingCta action="Continue" disabled={disableCta} />
       </Grid>
     </>
   );
